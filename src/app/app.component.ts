8a1b6fe2c3c9497bb18e357872a649dccdf83f76
sanzmiguel <sanzmartinmiguel@gmail.com>
2017-05-02 23:07:56 +0200

    Adding form and changing CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  
+  email: string;
+  password: string;
+
+  onSubmit(){
+      
+  }
+
 }
